8e7234eed38833e7fa54f8cb1caa908d4926fd91
skylerlee <skyler.ac.lee@gmail.com>
2025-03-16 21:24:15 +0800

    Update sha

--- ports/clevenshtein/portfile.cmake
+++ ports/clevenshtein/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO depotpkg/clevenshtein
     REF v1.0.2
-    SHA512 c1cb07284bb31bd5e1724aa169696a27b9633f1d35163107b1b0c6fda2cab098694a99e9f5f18e3ba4762217f85e445c32faf6dfabcaf723cbb19a186df79daf
+    SHA512 122278c85bb2e3dd88580f27aa30be609740ff4a23681abd37faee6e811ac6086cf489987d48f816803dc6eabbeef10c55c7086d9d1be31b50dc830c2165824b
     HEAD_REF master
 )
 
